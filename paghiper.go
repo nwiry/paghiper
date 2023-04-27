61ad3e9fbb9d8c268acaf240d78ed47f68f6ad49
Diogo  Cerqueira <diogodeveloper@hotmail.com>
2023-04-27 13:01:42 -0300

    Function for creating a transaction fixed in the payment method package

--- paghiper.go
+++ paghiper.go
@@ -1,38 +1,28 @@
 package paghiper
 
 import (
-	"encoding/json"
 	"errors"
 	"strings"
 
-	"github.com/nwiry/paghiper/exception"
-	"github.com/nwiry/paghiper/pix"
+	"github.com/nwiry/paghiper/payer"
 )
 
 // BOLETO_ENDPOINT is the endpoint URL for boleto requests.
 const BOLETO_ENDPOINT string = "https://api.paghiper.com/invoice/"
 
-// PIX_ENDPOINT is the endpoint URL for pix requests.
-const PIX_ENDPOINT string = "https://pix.paghiper.com/invoice/"
-
-// CREATE_PREFIX is the prefix for create requests.
-const CREATE_PREFIX string = "create/"
-
-// NOTIFICATION_PREFIX is the prefix for notification requests.
-const NOTIFICATION_PREFIX string = "notification/"
-
-// paymentRequest is an interface for a payment request.
-type paymentRequest interface {
-	// ToJSON converts the payment request to JSON.
-	ToJSON() ([]byte, error)
-}
-
-// PaghiperRequest represents a payment request to be sent to the PagHiper payment gateway.
-// It contains all necessary information for the payment, such as the seller's API key,
-// due date, discount amount, and order details.
 type PaghiperRequest struct {
 	// ApiKey is used to identify the seller.
 	ApiKey string `json:"apiKey"`
+
+	// Token is used to identify the seller's transaction.
+	Token string `json:"token,omitempty"`
+}
+
+// PaghiperPaymentRequest represents a payment request to be sent to the PagHiper payment gateway.
+// It contains all necessary information for the payment, such as the seller's API key,
+// due date, discount amount, and order details.
+type PaghiperPaymentRequest struct {
+	PaghiperRequest
 
 	// DaysDueDate sets the number of calendar days until expiration.
 	DaysDueDate int32 `json:"days_due_date"`
@@ -66,80 +56,33 @@ type PaghiperRequest struct {
 
 // ValidatePaymentRequest checks if all required fields in the payment request are filled.
 // Returns an error if any required field is missing.
-func (pr *PaghiperRequest) ValidatePaymentRequest() error {
+func (r *PaghiperPaymentRequest) ValidatePaymentRequest() error {
 	// Check if ApiKey is set
-	if strings.TrimSpace(pr.ApiKey) == "" {
+	if strings.TrimSpace(r.ApiKey) == "" {
 		return errors.New("ApiKey is required")
 	}
 
 	// Check if DaysDueDate is set
-	if pr.DaysDueDate <= 0 {
+	if r.DaysDueDate <= 0 {
 		return errors.New("DaysDueDate must be greater than zero")
 	}
 
 	// Check if NotificationURL is set
-	if strings.TrimSpace(pr.NotificationURL) == "" {
+	if strings.TrimSpace(r.NotificationURL) == "" {
 		return errors.New("NotificationURL is required")
 	}
 
 	// Check if OrderID is set
-	if strings.TrimSpace(pr.OrderID) == "" {
+	if strings.TrimSpace(r.OrderID) == "" {
 		return errors.New("OrderID is required")
 	}
 
 	return nil
 }
 
-// ToJSON converts the payment request to JSON.
-func (r *PaghiperRequest) ToJSON() ([]byte, error) {
-	return json.Marshal(r)
-}
-
-// NewPixRequest creates a new PIX payment request in Paghiper using the given `PaghiperRequest` and `PixRequest` objects.
-// It returns a `PixResponse` object and a `PaghiperError` object in case of an error, or `nil` values for both in case of success.
-func NewPixRequest(r *PaghiperRequest, p *pix.PixRequest) (*pix.PixResponse, *exception.PaghiperError, error) {
-	// Validate the payment request and the PIX request using the validation functions defined in the respective structs.
-	for _, f := range []func() error{
-		r.ValidatePaymentRequest,
-		p.ValidatePixRequest,
-		p.ValidatePayer,
-	} {
-		if err := f(); err != nil {
-			return nil, nil, err
-		}
-	}
-
-	// Create a new map to hold the JSON data of the payment request and PIX request.
-	m := make(map[string]interface{})
-
-	// Convert the payment request and PIX request objects to JSON and add them to the map.
-	var j []paymentRequest
-	j = append(j, r, p)
-	for _, j := range j {
-		b, err := j.ToJSON()
-		if err != nil {
-			return nil, nil, err
-		}
-		if err = json.Unmarshal(b, &m); err != nil {
-			return nil, nil, err
-		}
-	}
-
-	// Make a request to create a new PIX payment using the data in the map.
-	res, code, err := makeRequest(PIX_ENDPOINT, CREATE_PREFIX, m)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	// Validate the response and return a PaghiperError object if the response is not valid.
-	if err := validateRequestResponse(res, code); err != nil {
-		return nil, &exception.PaghiperError{ResponseMessage: err.Error(), HTTPCode: code}, nil
-	}
-
-	// Create a new PixResponse object using the data in the response and return it.
-	pixRes := &pix.PixResponse{}
-	if err := setResponse(res, pixRes); err != nil {
-		return nil, nil, err
-	}
-	return pixRes, nil, nil
+// NewPayer Ensures the import of the brdoc package by creating a new instance of payer
+func NewPayer() *payer.Payer {
+	p := &payer.Payer{}
+	p.ValidatePayer()
+	return p
 }
--- pix/pix.go
+++ pix/pix.go
@@ -4,12 +4,20 @@ import (
 	"encoding/json"
 	"errors"
 
+	"github.com/nwiry/paghiper"
+	"github.com/nwiry/paghiper/exception"
 	"github.com/nwiry/paghiper/items"
 	"github.com/nwiry/paghiper/payer"
+	"github.com/nwiry/paghiper/request"
 )
+
+// PIX_ENDPOINT is the endpoint URL for pix requests.
+const PIX_ENDPOINT string = "https://pix.paghiper.com/invoice/"
 
 // PixRequest represents a request for a Pix payment.
 type PixRequest struct {
+	// PaghiperPaymentRequest contains information about seller and transaction
+	paghiper.PaghiperPaymentRequest
 	// Payer contains information about the person making the payment.
 	payer.Payer
 	// Items contains a list of items included in the payment request.
@@ -18,7 +26,7 @@ type PixRequest struct {
 
 // validatePixRequest checks if a PixRequest contains at least one item.
 // Returns an error if the items list is empty, otherwise returns nil.
-func (r *PixRequest) ValidatePixRequest() error {
+func (r *PixRequest) ValidateCreatePixRequest() error {
 	// Check if the length of the items list is zero
 	if len(r.Items) == 0 {
 		// If it is, return an error with a message
@@ -29,5 +37,51 @@ func (r *PixRequest) ValidatePixRequest() error {
 }
 
 func (p *PixRequest) ToJSON() ([]byte, error) {
-    return json.Marshal(p)
+	return json.Marshal(p)
+}
+
+// Create creates a new PIX payment request in Paghiper using the given `PaghiperRequest` and `PixRequest` objects.
+// It returns a `PixResponse` object and a `PaghiperError` object in case of an error, or `nil` values for both in case of success.
+func (p *PixRequest) Create() (*PixResponse, *exception.PaghiperError, error) {
+	// Validate the payment request and the PIX request using the validation functions defined in the respective structs.
+	for _, f := range []func() error{
+		p.ValidatePaymentRequest,
+		p.ValidatePayer,
+		p.ValidateCreatePixRequest,
+	} {
+		if err := f(); err != nil {
+			return nil, nil, err
+		}
+	}
+
+	// Create a new map to hold the JSON data of the payment request and PIX request.
+	m := make(map[string]interface{})
+
+	// Convert the payment request and PIX request objects to JSON and add them to the map.
+	b, err := p.ToJSON()
+	if err != nil {
+		return nil, nil, err
+	}
+	if err = json.Unmarshal(b, &m); err != nil {
+		return nil, nil, err
+	}
+
+	// Make a request to create a new PIX payment using the data in the map.
+	res, code, err := request.Make(PIX_ENDPOINT, "create/", m)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// Validate the response and return a PaghiperError object if the response is not valid.
+	if err := request.ValidateRequestResponse(res, code); err != nil {
+		return nil, &exception.PaghiperError{ResponseMessage: err.Error(), HTTPCode: code}, nil
+	}
+
+	// Create a new PixResponse object using the data in the response and return it.
+	pixRes := &PixResponse{}
+	if err := request.SetResponse(res, pixRes); err != nil {
+		return nil, nil, err
+	}
+
+	return pixRes, nil, nil
 }
